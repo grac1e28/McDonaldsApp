6ad41f4d8b796201564f963a7a4f6280f0eaead0
Gracie Maeks <graciemarks@exe-coll.ac.uk>
2025-09-27 20:29:22 +0100

    Site up with header styling and 2 linked pages.

---
+++ McDonalds App/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+def csv_categories():
+    df = pd.read_csv("mcdonalds.csv")
+    df = df.fillna("")
+    categories = df["Category"].unique()
+    return categories
+
+def csv_items():
+    df = pd.read_csv("mcdonalds.csv")
+    items = []
+    for cat in csv_categories():
+    #if category == cat:
+        rows = df.values.tolist()
+        for row in rows:
+            if row[0] == cat:
+                items.append(row[1])
+    return items
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Breakfast')
+def breakfast():
+    return render_template('Breakfast.html', categories = csv_items())
+
+@app.route('/Beef-and-pork')
+def beef_and_pork():
+    return render_template('Beef-and-pork.html', categories = csv_items())
+
+@app.route('/Chicken-and-fish')
+def chicken_and_fish():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Salads')
+def salads():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Snacks-and-sides')
+def snacks_and_sides():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Desserts')
+def desserts():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Beverages')
+def beverages():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Coffee-and-tea')
+def coffee_and_tea():
+    return render_template('index.html', categories = csv_items())
+
+@app.route('/Smoothies-and-shakes')
+def smoothies_and_shakes():
+    return render_template('index.html', categories = csv_items())
+
+if __name__ == '__main__':
+    app.run(debug=True)
